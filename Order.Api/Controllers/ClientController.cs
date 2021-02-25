9a5e8d753e6ea401020974329da4f7347120d9dc
EdwardCr <cristiancrtz556@gmail.com>
2021-02-25 10:26:06 -0600

    obtener usuario

---
+++ Order.Api/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Order.DAL;
+using Order.DTO;
+
+namespace Order.Api.Controllers
+{
+    [ApiController]
+    [Route("client")]
+    public class ClientController : ControllerBase
+    {
+
+        private ClienteDal clientedb =new ClienteDal();
+        
+        [HttpGet("{id}")]
+        public  IActionResult Get(int id)
+        {
+           
+            var resultado=clientedb.Obtener(id);  
+          
+            if(resultado.ClienteId==0){
+                return NotFound();
+            }else{
+                return Ok(resultado);
+            }
+
+            
+        }
+    }
+}
--- Order.DAL/ClienteDal.cs
+++ Order.DAL/ClienteDal.cs
@@ -6,21 +6,56 @@ using System.Data.SqlClient;
 
 namespace Order.DAL
 {
-    public class ClienteDal:ICliente
+    public class ClienteDal : ICliente
     {
+        public ClienteDTO Actualizar()
+        {
+            throw new NotImplementedException();
+        }
 
-        private SqlConnection con = new SqlConnection("");
-        public  ClienteDTO Obtener(int id){
-            return new ClienteDTO();
-         }
-       public int Registrar(ClienteDTO cliente){
-            return 1;
+        public int Eliminar(int id)
+        {
+            throw new NotImplementedException();
         }
-       public  ClienteDTO Actualizar(){
-            return new ClienteDTO();
+
+        public ClienteDTO Obtener(int id)
+        {
+            ClienteDTO cliente = new ClienteDTO();
+
+            try
+            {
+                using (var con = new SqlConnection("Server=localhost;Database=Ordenes;Trusted_Connection=True;MultipleActiveResultSets=true"))
+                {
+                    con.Open();
+
+                    var query = new SqlCommand("SELECT * FROM Cliente WHERE ClienteId = @id", con);
+                    query.Parameters.AddWithValue("@id", id);
+
+                    using (var dr = query.ExecuteReader())
+                    {
+                        dr.Read();
+                        if (dr.HasRows) {
+                            cliente.ClienteId = Convert.ToInt32(dr["ClienteId"]);
+                            cliente.Nombres = dr["Nombres"].ToString();
+                            cliente.Apellidos = dr["Apellidos"].ToString();
+                            cliente.Fecha_Nacimiento = Convert.ToDateTime(dr["Fecha_nacimiento"]);
+                            cliente.telefono=dr["telefono"].ToString();
+                            cliente.email = dr["email"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return cliente;
         }
-        public int Eliminar(int id){
-            return 1;
+
+        public int Registrar(ClienteDTO cliente)
+        {
+            throw new NotImplementedException();
         }
     }
 }
